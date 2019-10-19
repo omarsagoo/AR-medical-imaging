41b238594607c6b674520bd687ea6e2fdafba59f
omarsagoo <om.sagoo@gmail.com>
2019-10-18 18:51:01 -0700

    adds comments to app.py

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 from flask_pymongo import PyMongo
 
+'''Sets the variables for URI and Uploads'''
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'dcm'])
 UPLOADS_FOLDER = '/Users/omarsagoo/dev/courses/intensive/med-imaging/static/files/uploads'
 URI = 'mongodb://localhost:27017/MedImaging'
 
+''' Variables for app configuration and creating the DB'''
 app = Flask(__name__)
 app.config["UPLOADS_FOLDER"] = UPLOADS_FOLDER
 app.config["ALLOWED_EXT"] = ALLOWED_EXTENSIONS
@@ -24,22 +26,25 @@ patients = db.patients
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    '''creates the route for the uploaded files to be displayed '''
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
 @app.route('/')
 def index():
+    '''Main homepage displays all the patients'''
     read_patients = patients.find()
 
     return render_template('index.html', patients=read_patients)
 
 @app.route('/patients/new')
 def patient_new():
-
+    '''returns the teemplate for creating a new patient '''
     return render_template('patient_new.html', patient={}, title="New Patient")
 
 @app.route('/patients', methods=['POST'])
 def patient_submit():
+    '''submits the patient into the DB and redirects back to patient show to display the information'''
     patient = {
         'name': request.form.get('name'),
         'dob': request.form.get('dob'),
@@ -52,6 +57,7 @@ def patient_submit():
 
 @app.route('/patients/<patient_id>')
 def patient_show(patient_id):
+    ''' shows the patients on the page '''
     patient = patients.find_one({'_id': ObjectId(patient_id)})
     patient_files = medfiles.find({'patient_id': ObjectId(patient_id)})
 
@@ -59,11 +65,13 @@ def patient_show(patient_id):
 
 @app.route('/patients/<patient_id>/edit')
 def patient_edit(patient_id):
+    '''allows editing of the database files of the patients'''
     patient = patients.find_one({'_id': ObjectId(patient_id)})
     return render_template('patient_edit.html', patient=patient, title='Edit Patient')
 
 @app.route('/patients/<patient_id>', methods=['POST'])
 def patient_update(patient_id):
+    '''updates the patients in the database with patient_edit'''
     updated_patient = {
         'name': request.form.get('name'),
         'dob': request.form.get('dob'),
@@ -77,11 +85,13 @@ def patient_update(patient_id):
 
 @app.route('/patients/<patient_id>/delete', methods=['POST'])
 def patient_delete(patient_id):
+    '''deletes a patient from the database'''
     patients.delete_one({'_id': ObjectId(patient_id)})
 
     return redirect(url_for('index'))
 
 def allowed_file(filename):
+    ''' makes sure that files with the allowed extensions can be inputed only '''
     if not '.' in filename:
         return False
     
@@ -94,7 +104,7 @@ def allowed_file(filename):
 
 @app.route('/patients/files', methods=['POST'])
 def files_new():
-
+    '''uploads the files into the uploads file, and makes sure the files are secure, have a name and are allowed '''
     if request.method == 'POST':
         if request.files:
             med_file = request.files['file']
@@ -111,7 +121,7 @@ def files_new():
                 med_file.save(os.path.join(app.config["UPLOADS_FOLDER"], filename))
 
             print('image saved')
-
+            ''' creates the medfile database structure '''
             medfile = {
                 'type': request.form.get('type'),
                 'name': request.form.get('name'),
@@ -126,6 +136,7 @@ def files_new():
 
 @app.route('/patients/files/<medfile_id>', methods=['POST'])
 def files_delete(medfile_id):
+    ''' deletes a file from the database '''
     medfile = medfiles.find_one({'_id': ObjectId(medfile_id)})
     medfiles.delete_one({'_id': ObjectId(medfile_id)})
     return redirect(url_for('patient_show', patient_id=medfile.get('patient_id')))
